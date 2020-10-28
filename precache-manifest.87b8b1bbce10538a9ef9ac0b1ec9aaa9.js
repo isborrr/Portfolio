f5a2714afae7f954646fd765076067a63d041d34
Ismael Boro <ismaelboro@Ismaels-MBP.home>
2020-10-27 22:55:20 -0400

    Updates

---
+++ precache-manifest.87b8b1bbce10538a9ef9ac0b1ec9aaa9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c930dbb953cc770e560d1cbf0dea492",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "740a965ea577bad3eef6",
+    "url": "/myportfolio/static/css/main.fdf28208.chunk.css"
+  },
+  {
+    "revision": "c6cd186d597e8254285c",
+    "url": "/myportfolio/static/js/2.91f34e12.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/myportfolio/static/js/2.91f34e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740a965ea577bad3eef6",
+    "url": "/myportfolio/static/js/main.33f2ccfd.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  },
+  {
+    "revision": "b2317448a6ca9069cee581d1b91713b8",
+    "url": "/myportfolio/static/media/graduation.b2317448.jpg"
+  },
+  {
+    "revision": "7638681598696e8c62e866327eda0cfd",
+    "url": "/myportfolio/static/media/image.76386815.jpg"
+  },
+  {
+    "revision": "2abdf5d0354d31da56375efb10662613",
+    "url": "/myportfolio/static/media/linkdln.2abdf5d0.png"
+  },
+  {
+    "revision": "71f3d9c143014929774ecfd093556899",
+    "url": "/myportfolio/static/media/mail.71f3d9c1.png"
+  }
+]);
